33532b648fdcd1ef87116111a4e8e8d6005c40c0
windfly <974589985@qq.com>
2020-09-10 13:51:25 +0800

    添加@pokemon组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import PmButton from '@pokemon-ui/button'
+import '@pokemon-ui/button/dist/es/index.css'
 
+Vue.use(PmButton)
 Vue.config.productionTip = false
 
 new Vue({
