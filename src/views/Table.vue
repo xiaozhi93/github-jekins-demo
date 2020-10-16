e4583beb57840a57a28bd0bd1e7ecedd86d428ef
windfly <974589985@qq.com>
2020-10-16 17:55:58 +0800

    vue-cli3默认是支持jsx的

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,5 +1,9 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script>
+export default {
+  render() {
+    return (
+      <h1>11111</h1>
+    )
+  }
+}
+</script>
