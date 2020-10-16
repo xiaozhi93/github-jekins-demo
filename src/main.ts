520062eb56ece8741db37ace9c3a6f8b5a0d839e
windfly <974589985@qq.com>
2020-10-16 17:51:20 +0800

    重写element-ui的table组件

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import PmButton from '@pokemon-ui/button'
 import '@pokemon-ui/button/dist/es/index.css'
 // import PmTablePro from '@pokemon-ui/table-pro'
 const PmTablePro = require('@pokemon-ui/table-pro')
-console.log(PmTablePro.default)
+// console.log(PmTablePro.default)
 import elementUi from "element-ui";
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import store from './store'
 // import TestModule from '@pokemon-ui/test-module'
-const TestModule = require('@pokemon-ui/test-module')
-console.log(TestModule, 'test')
+// const TestModule = require('@pokemon-ui/test-module')
+// console.log(TestModule, 'test')
 
 Vue.use(PmButton)
 Vue.use(PmTablePro.default)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-table-new :data="tableData" style="width: 100%">
+      <el-table-column-new prop="date" label="日期" width="180">
+      </el-table-column-new>
+      <el-table-column-new prop="name" label="姓名" width="180">
+      </el-table-column-new>
+      <el-table-column-new prop="address" label="地址"> </el-table-column-new>
+    </el-table-new>
   </div>
 </template>
+<script>
+import ElTableNew from "@/components/table/src/table.vue";
+import ElTableColumnNew from "@/components/table/src/table-column";
+export default {
+  components: {
+    ElTableNew,
+    ElTableColumnNew,
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+};
+</script>
