ae1a9cc4aea604fc49f3545307a02f26875ad03e
windfly <974589985@qq.com>
2020-10-20 18:08:40 +0800

    实践选择器组件

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,18 +8,38 @@
       </el-table-column-new>
       <el-table-column-new prop="address" label="地址"> </el-table-column-new>
     </el-table-new>
+    <el-date-picker-new
+      v-model="value1"
+      type="date"
+      placeholder="选择日期">
+    </el-date-picker-new>
+    <el-date-picker-new
+      v-model="value2"
+      type="month"
+      placeholder="选择日期">
+    </el-date-picker-new>
+    <el-date-picker-new
+      v-model="value3"
+      type="week"
+      placeholder="选择周">
+    </el-date-picker-new>
   </div>
 </template>
 <script>
 import ElTableNew from "@/components/table/src/table.vue";
 import ElTableColumnNew from "@/components/table/src/table-column";
+import ElDatePickerNew from "@/components/date-picker/src/picker/date-picker";
 export default {
   components: {
     ElTableNew,
     ElTableColumnNew,
+    ElDatePickerNew
   },
   data() {
     return {
+      value1: '',
+      value2: '',
+      value3: '',
       tableData: [
         {
           date: "2016-05-02",
