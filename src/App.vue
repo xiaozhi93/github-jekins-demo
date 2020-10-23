4a38e1107168d63b3cf29c2821f37a4d5650dcea
windfly <974589985@qq.com>
2020-10-23 10:26:44 +0800

    使用el-scrollbar模拟滚动然后scrollBehavior不生效

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
+    <el-scrollbar style="height: 100%">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/table">Table</router-link>
     </div>
-    <router-view/>
+      <router-view/>
+    </el-scrollbar>
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+  height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 
 #nav {
